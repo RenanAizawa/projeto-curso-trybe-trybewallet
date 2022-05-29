97188b4b31a69d8786d2a31ce4efe962a943d259
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-05-28 21:26:06 -0400

    requesitos 7 e 8, concluidos

--- src/components/TabelaDeControle.jsx
+++ src/components/TabelaDeControle.jsx
@@ -1,10 +1,77 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 
 class TabelaDeControle extends React.Component {
+  listaDasDespesas = () => {
+    const { lista } = this.props;
+    if (lista.length > 0) {
+      const desconstruçao = lista.map((desp) => ({
+        id: desp.id,
+        descrição: desp.description,
+        tag: desp.tag,
+        metodoPag: desp.method,
+        cifrao: desp.currency,
+        valor: Number(desp.value).toFixed(2),
+        moeda: desp.currency,
+        cambioUtil: desp.exchangeRates[desp.currency].name,
+        valorCombio: Number(desp.exchangeRates[desp.currency].ask).toFixed(2),
+        valorConv: (Number(desp
+          .exchangeRates[desp.currency].ask) * Number(desp.value)).toFixed(2),
+        MoedaConv: 'Real',
+      }));
+      return desconstruçao.map((desp) => (
+        <tr key={ desp.id }>
+          <td>{desp.descrição}</td>
+          <td>{desp.tag}</td>
+          <td>{desp.metodoPag}</td>
+          <td>
+            {/* {desp.cifrao} */}
+            {desp.valor}
+          </td>
+          <td>{desp.moeda}</td>
+          <td>{desp.cambioUtil}</td>
+          <td>{desp.valorCombio}</td>
+          <td>{desp.valorConv}</td>
+          <td>{desp.MoedaConv}</td>
+          <td>
+            <button type="button">Excluir</button>
+            <button type="button">Editar</button>
+          </td>
+        </tr>
+      ));
+    }
+    return <h1>lista não criada</h1>;
+  }
+
   render() {
-    return (<h1>Componente Tabela</h1>);
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+          {this.listaDasDespesas()}
+        </tbody>
+      </table>
+    );
   }
 }
 
-export default connect()(TabelaDeControle);
+const mapStateToProps = (state) => ({
+  lista: state.wallet.expenses,
+});
+
+TabelaDeControle.propTypes = {
+  lista: PropTypes.array,
+}.isRequired;
+
+export default connect(mapStateToProps)(TabelaDeControle);
